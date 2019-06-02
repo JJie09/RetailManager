d50b5cf18aa669aba2a75fdef15d17dcd6e6a990
JJ <jjie0921@gmail.com>
2019-06-02 16:50:44 +0800

    Add Library Project for accessing database
    Adds generic method to query database. Get Id from token and return User information in UserModel

---
+++ RMDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,38 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMDataManager.Library.Internal.DataAccess
+{
+    internal class SqlDataAccess
+    {
+        public string GetConnectionString(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+
+        public List<T> LoadData<T, U>(string storedProcedured, U parameters, string connectionStringName)
+        {
+            string connectionString = GetConnectionString(connectionStringName);
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                List<T> rows = connection.Query<T>(storedProcedured, parameters, commandType: CommandType.StoredProcedure).ToList();
+                return rows;
+            }
+        }
+        public void SaveData<T>(string storedProcedured, T parameters, string connectionStringName)
+        {
+            string connectionString = GetConnectionString(connectionStringName);
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Execute(storedProcedured, parameters, commandType: CommandType.StoredProcedure);
+            }
+        }
+    }
+}
---
+++ RMDataManager/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNet.Identity;
+using RMDataManager.Library.DataAccess;
+using RMDataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace RMDataManager.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // GET: User/Details/5
+        public List<UserModel> GetById()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+            UserData data = new UserData();
+
+            return data.GetUserById(userId);
+        }
+    }
+}
