db289ea95c8b96a312a32a7d18cbb8ba3c24ef45
JJ <jjie0921@gmail.com>
2019-06-03 22:59:51 +0800

    Add Sales Page
    Adds SalesViewModel, SalesView

---
+++ RMDesktopUI/ViewModels/SalesViewModel.cs
@@ -0,0 +1,95 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMDesktopUI.ViewModels
+{
+    public class SalesViewModel : Screen
+    {
+        private BindingList<string> _products;
+        private BindingList<string> _cart;
+        private int _itemQuantity;
+
+        public BindingList<string> Products
+        {
+            get { return _products; }
+            set
+            {
+                _products = value;
+                NotifyOfPropertyChange(() => Products);
+            }
+        }
+        public int ItemQuantity
+        {
+            get { return _itemQuantity; }
+            set
+            {
+                _itemQuantity = value;
+                NotifyOfPropertyChange(() => Products);
+            }
+        }
+        public BindingList<string> Cart
+        {
+            get { return _cart; }
+            set
+            {
+                _cart = value;
+                NotifyOfPropertyChange(() => Cart);
+            }
+        }
+
+        public string SubTotal
+        {
+            get { return "$0.00"; }
+        }
+        public string Tax
+        {
+            get { return "$0.00"; }
+        }
+        public string Total
+        {
+            get { return "$0.00"; }
+        }
+
+
+        public bool CanAddToCart
+        {
+            get
+            {
+                return false;
+            }
+        }
+        public void AddToCart()
+        {
+
+        }
+
+        public bool CanRemoveFromCart
+        {
+            get
+            {
+                return false;
+            }
+        }
+        public void RemoveFromCart()
+        {
+
+        }
+
+        public bool CanCheckout
+        {
+            get
+            {
+                return false;
+            }
+        }
+        public void CheckOut()
+        {
+
+        }
+    }
+}
